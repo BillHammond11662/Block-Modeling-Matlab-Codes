5ad1100e069375daf92d963a52dcd7313147f83c
Bill Hammond <156459790+BillHammond11662@users.noreply.github.com>
2024-01-18 16:05:30 -0800

    Delete blockcodes directory

--- blockcodes/Block2Nodes.m
+++
@@ -1,7 +0,0 @@
-function [nds]=Block2Nodes(blocks,bnum)
-%
-% nds = Block2Nodes(blocks,bnum);
-
-blocknames = fieldnames(blocks);
-%eval(['nds=blocks.' char(blocknames(bnum)) ';']);
-nds = blocks.(char(blocknames(bnum)));
